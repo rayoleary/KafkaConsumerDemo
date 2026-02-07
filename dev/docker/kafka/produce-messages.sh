7edddd91f17ce9365d9947a7e5551916b7a65329
Ray O'Leary <ray.oleary@gmail.com>
2026-02-07 09:37:57 -0600

    Specify a partition key when publishing messages to ensure correct arrival order

--- dev/docker/kafka/produce-messages.sh
+++ dev/docker/kafka/produce-messages.sh
@@ -5,7 +5,7 @@ echo "Publishing 20 messages to DEMO topic..."
 for i in {1..20}
 do
   MESSAGE="{\"userName\":\"user${i}\",\"userId\":\"id-${i}\"}"
-  echo "$MESSAGE" | kafka-console-producer --bootstrap-server localhost:9092 --topic DEMO
+  echo "user${i}:${MESSAGE}" | kafka-console-producer --bootstrap-server localhost:9092 --topic DEMO --property "parse.key=true" --property "key.separator=:"
   echo "Published message $i: $MESSAGE"
 done
 
